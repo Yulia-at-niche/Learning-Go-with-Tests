44abcadb5070483c7da85db9c709891926811721
Yulia-at-Niche <yulia.brooks@niche.com>
2025-08-08 16:45:37 -0400

    Refactor greeting constants and extract greetingPrefix function for better readability

--- hello.go
+++ hello.go
@@ -2,21 +2,27 @@ package main
 
 import "fmt"
 
-const spanish = "Spanish"
-const french = "French"
-const klingon = "Klingon"
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
-const klingonHelloPrefix = "nuqneH, "
+const (
+	spanish = "Spanish"
+	french  = "French"
+	klingon = "Klingon"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+	klingonHelloPrefix = "nuqneH, "
+)
 
 func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
+	prefix := greetingPrefix(language)
 
-	prefix := englishHelloPrefix
+	return prefix + name
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case spanish:
 		prefix = spanishHelloPrefix
@@ -24,8 +30,10 @@ func Hello(name, language string) string {
 		prefix = frenchHelloPrefix
 	case klingon:
 		prefix = klingonHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	return prefix + name
+	return
 }
 
 func main() {
