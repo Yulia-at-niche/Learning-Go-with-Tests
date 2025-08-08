2047904163fa68f66c24f32b184fc61a4c9a1789
Yulia-at-Niche <yulia.brooks@niche.com>
2025-08-08 15:56:09 -0400

    test refactor, make a case

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,12 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Narla")
-	want := "Hello, Narla"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Narla")
+		want := "Hello, Narla"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
