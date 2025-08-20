082f9b1d0653722b45d847f2f226461c0b53cfcb
Yulia-at-Niche <yulia.brooks@niche.com>
2025-08-20 16:11:01 -0400

    make the loop more efficient string type

--- iteration/repeater.go
+++ iteration/repeater.go
@@ -3,13 +3,9 @@ package iteration
 import "strings"
 
 func Repeater(s string) string {
-	// var repeated string
 	var repeated strings.Builder
 	for i := 0; i < 5; i++ {
-		// Plus And operator. Same as: repeated = repeated + s
-		// repeated += s
 		repeated.WriteString(s)
 	}
-	// return repeated
 	return repeated.String()
 }
