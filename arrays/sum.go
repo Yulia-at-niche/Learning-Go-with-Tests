1540bc67aa595f5d7487dbeffd2fe413a71c0109
Yulia-at-Niche <yulia.brooks@niche.com>
2025-08-21 14:19:12 -0400

    refactor by using range

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,10 +1,11 @@
 package main
 
 func Sum(numbers [5]int) int {
-	var sum int
+	sum := 0
 
-	for i := 0; i < len(numbers); i++ {
-		sum += numbers[i]
+	for _, num := range numbers {
+		sum += num
 	}
+
 	return sum
 }
