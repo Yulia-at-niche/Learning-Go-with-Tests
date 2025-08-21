136127e8d8e786092aa893ad216725aae920b633
Yulia-at-Niche <yulia.brooks@niche.com>
2025-08-21 16:23:31 -0400

    doc

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,5 +1,6 @@
 package main
 
+// Adds all elements of a slice and returns the total
 func Sum(numbers []int) int {
 	sum := 0
 
@@ -10,6 +11,7 @@ func Sum(numbers []int) int {
 	return sum
 }
 
+// Takes many slices, finds the sum of all the elements, and returns the sums of each slice as a single slice
 func SumAll(slices ...[]int) []int {
 	var sums []int
 	for _, slice := range slices {
