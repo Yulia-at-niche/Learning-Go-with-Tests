b58290a46c9121b1016d566bb3ae02640ab2d72f
Yulia-at-Niche <yulia.brooks@niche.com>
2025-08-22 10:05:43 -0400

    Make a rectangle struct and use it for tests and functions

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -3,18 +3,19 @@ package structs
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	t.Run("perimeter", func(t *testing.T) {
-		got := Perimeter(10.0, 10.0)
-		want := 40.0
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
 
-		if got != want {
-			t.Errorf("got %.2f, want %.2f", got, want)
-		}
-	})
+	if got != want {
+		t.Errorf("got %.2f, want %.2f", got, want)
+	}
+
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 5.5)
+	rectangle := Rectangle{10.0, 5.5}
+	got := Area(rectangle)
 	want := 55.0
 
 	if got != want {
