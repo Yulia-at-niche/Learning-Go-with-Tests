d76ccb14c6e47fcebc2a05ea70e85a770f6278ff
Yulia-at-Niche <yulia.brooks@niche.com>
2025-08-22 09:19:41 -0400

    refactor tests with a helper function. Setting functions to variables

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -33,16 +33,20 @@ func TestSumAll(t *testing.T) {
 }
 
 func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	}
 	t.Run("sum tails of slices", func(t *testing.T) {
 		sliceOne := []int{1, 2, 8} // 10
 		sliceTwo := []int{1, 10}   // 10
 
 		got := SumAllTails(sliceOne, sliceTwo)
 		want := []int{10, 10}
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %d, want %d, given %v and %v", got, want, sliceOne, sliceTwo)
-		}
+		checkSums(t, got, want)
 	})
 	t.Run("safely sum empty slices", func(t *testing.T) {
 		sliceOne := []int{1, 2, 8} // 10
@@ -51,8 +55,6 @@ func TestSumAllTails(t *testing.T) {
 		got := SumAllTails(sliceOne, sliceTwo)
 		want := []int{10, 0}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %d, want %d, given %v and %v", got, want, sliceOne, sliceTwo)
-		}
+		checkSums(t, got, want)
 	})
 }
