bd39323e12c75ec0e2f15128232a981766dbbd1f
Yulia-at-Niche <yulia.brooks@niche.com>
2025-08-22 09:56:02 -0400

    test and function for area

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,6 +1,9 @@
 package structs
 
-// Find the perimeter of a rectangle
 func Perimeter(width float64, height float64) float64 {
 	return 2 * (width + height)
+}
+
+func Area(width, length float64) float64 {
+	return width * length
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -3,7 +3,7 @@ package structs
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	t.Run("", func(t *testing.T) {
+	t.Run("perimeter", func(t *testing.T) {
 		got := Perimeter(10.0, 10.0)
 		want := 40.0
 
@@ -11,4 +11,13 @@ func TestPerimeter(t *testing.T) {
 			t.Errorf("got %.2f, want %.2f", got, want)
 		}
 	})
+}
+
+func TestArea(t *testing.T) {
+	got := Area(10.0, 5.5)
+	want := 55.0
+
+	if got != want {
+		t.Errorf("got %.2f, want %.2f", got, want)
+	}
 }
