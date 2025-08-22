6a85143469e8020b8e69a27c21a902486edbef1f
Yulia-at-Niche <yulia.brooks@niche.com>
2025-08-22 14:53:37 -0400

    start wallet tests and wallet struct. Just enough to avoid compiler errors

---
+++ pointers/wallet.go
@@ -0,0 +1,10 @@
+package pointers
+
+type Wallet struct{}
+
+func (w Wallet) Deposit(int) {
+
+}
+func (w Wallet) Balance() int {
+	return 0
+}
---
+++ pointers/wallet_test.go
@@ -0,0 +1,17 @@
+package pointers
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	// To keep the struct's fields private and safe from outside influence we're not setting any values here, nor are we accessing them directly.
+	wallet := Wallet{}
+
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+	want := 10
+
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
