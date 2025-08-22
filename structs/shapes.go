c501fd96f06d3035ac4bdfa113d358eead252029
Yulia-at-Niche <yulia.brooks@niche.com>
2025-08-22 13:05:24 -0400

    Add a new shape to the test, then add the struct and method to support it

--- structs/shapes.go
+++ structs/shapes.go
@@ -52,10 +52,15 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+type Triangle struct {
+	Base   float64
+	Height float64
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
