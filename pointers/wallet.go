d80f6a354f697f71abd469d3f18df7ac1b73985f
Yulia-at-Niche <yulia.brooks@niche.com>
2025-09-12 11:35:14 -0400

    a self written definition of pointers

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -31,3 +31,17 @@ func (w *Wallet) Withdraw(amount Bitcoin) error {
 	w.balance -= amount
 	return nil
 }
+
+/*
+	A pointer is basically an address. Specifically, it's a variable that stores the memory address of where some data lives.
+
+	A pointer points to the thing. It's an address to where the thing is in memory. This is similar to javascript reference types like objects and arrays.
+
+	The big difference between Go and JS is that in Go, the default is to copy. You need to explicitly say "No, use the reference i.e. pointer".
+
+	In JS it's the opposite. By default you access the reference and you need to explicitly copy and make a new reference (think of all the non mutating copying of state React and Redux)
+
+	* Go: "Copy by default, opt into sharing with pointers"
+
+	* JavaScript: "Share by default, opt into copying when needed"
+*/
