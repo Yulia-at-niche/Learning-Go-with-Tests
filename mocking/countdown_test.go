f1ea3e5a65c0c5ad0b7740ea0d7f85f08fef5c0d
Yulia-at-Niche <yulia.brooks@niche.com>
2025-09-19 16:47:24 -0400

    mocking: make a test to assert the correct order of operations

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
@@ -14,23 +15,60 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
+// This is another mock of the Sleeper. We need a struct to hold data that we can assert in tests
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+// This mock of the Sleep() function that the Sleeper interface stipulates. This one will append the word "sleep" to the Call field's array
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+// This mock of the Sleep() function that the Sleeper interface stipulates. This one will append the word "write" to the Call field's array
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	// since the return types are given names in the declaration using just 'return' is sufficient to implicitly return both values
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
+	t.Run("test output", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		spySleeper := &SpySleeper{}
 
-	// Countdown is going to call spySleeper.Sleep() instead of that delaying it's going to increment the Call field of the test's spySleeper
-	Countdown(buffer, spySleeper)
+		// Countdown is going to call spySleeper.Sleep() instead of that delaying it's going to increment the Call field of the test's spySleeper
+		Countdown(buffer, spySleeper)
 
-	got := buffer.String()
-	want := `3
+		got := buffer.String()
+		want := `3
 2
 1
 Go!`
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
-	if spySleeper.Calls != 3 {
-		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+		if spySleeper.Calls != 3 {
+			t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
+		}
+	})
+	t.Run("test order of calls: sleep is called before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			write, sleep,
+			write, sleep,
+			write, sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
 }
