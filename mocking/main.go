12a72a5005fb9c69e20997bfd963d2f7909663a2
Yulia-at-Niche <yulia.brooks@niche.com>
2025-09-19 15:40:25 -0400

    mocking: make a dramatic pause

--- mocking/main.go
+++ mocking/main.go
@@ -4,18 +4,22 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 func Countdown(out io.Writer) {
 	countdown := 3
+	finalWord := "Go!"
 	// while i is greater than 0, i will decrement by 1
 	// i is 3. First time i is 3, then 2, then 1 at which point the loop will decrement it to 0 and it won't run anymore.
 	for i := countdown; i > 0; i-- {
 		// This print function automatically appends a newline
 		fmt.Fprintln(out, i)
+		// Put a pause between each number
+		time.Sleep(1 * time.Second)
 	}
 	// This won't append a newline
-	fmt.Fprint(out, "Go!")
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
