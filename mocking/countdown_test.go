be7eacaaa8156a68c16aa5434a799b9871442348
Yulia-at-Niche <yulia.brooks@niche.com>
2025-09-19 16:24:23 -0400

    mocking: make a mock and implement it

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -5,10 +5,21 @@ import (
 	"testing"
 )
 
+type SpySleeper struct {
+	Calls int
+}
+
+// We're making a mock of the real Sleep() method. Instead of it being a way to delay the program from running, it will increment the Call field of the SpySleeper struct
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
 
-	Countdown(buffer)
+	// Countdown is going to call spySleeper.Sleep() instead of that delaying it's going to increment the Call field of the test's spySleeper
+	Countdown(buffer, spySleeper)
 
 	got := buffer.String()
 	want := `3
@@ -18,5 +29,8 @@ Go!`
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
 	}
 }
--- mocking/main.go
+++ mocking/main.go
@@ -7,21 +7,29 @@ import (
 	"time"
 )
 
-func Countdown(out io.Writer) {
+// Define an interface for sleeper. The argument for countdown will use this and so will the tests for their mocks
+type Sleeper interface {
+	Sleep()
+}
+
+// This is the 'real' sleeper. It will implement the Sleeper interface
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	countdown := 3
 	finalWord := "Go!"
-	// while i is greater than 0, i will decrement by 1
-	// i is 3. First time i is 3, then 2, then 1 at which point the loop will decrement it to 0 and it won't run anymore.
 	for i := countdown; i > 0; i-- {
-		// This print function automatically appends a newline
 		fmt.Fprintln(out, i)
-		// Put a pause between each number
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
-	// This won't append a newline
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
