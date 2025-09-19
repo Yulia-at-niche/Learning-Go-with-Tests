22cb3617f3514a515e74f1e1d62121a47d987203
Yulia-at-Niche <yulia.brooks@niche.com>
2025-09-19 15:36:47 -0400

    mocking: make the test and function count down for real

--- mocking/main.go
+++ mocking/main.go
@@ -7,7 +7,15 @@ import (
 )
 
 func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+	countdown := 3
+	// while i is greater than 0, i will decrement by 1
+	// i is 3. First time i is 3, then 2, then 1 at which point the loop will decrement it to 0 and it won't run anymore.
+	for i := countdown; i > 0; i-- {
+		// This print function automatically appends a newline
+		fmt.Fprintln(out, i)
+	}
+	// This won't append a newline
+	fmt.Fprint(out, "Go!")
 }
 
 func main() {
